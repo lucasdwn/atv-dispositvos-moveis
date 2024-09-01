7c8c32b28054205ca57d7b9298b084adc6c6f1f5
lucasdwn <lfcosta0804@gmail.com>
2024-09-01 18:52:30 -0300

    Exercicio 2 aula 2

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -3,8 +3,8 @@ import Constants from 'expo-constants';
 import logo from "../../assets/fatec.png";
 import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 import { RootStackParamList } from '../../types';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-interface Props extends NativeStackScreenProps<RootStackParamList, "Home"> {}
+import { DrawerScreenProps } from '@react-navigation/drawer';
+interface Props extends DrawerScreenProps<RootStackParamList, "Home"> { }
 function Logo() {
     return <Image source={logo} style={{ width: 140, height: 140, resizeMode: 'contain' }} />;
 }
@@ -18,71 +18,71 @@ const Home: React.FC<Props> = ({ navigation }) => {
                     <Text style={{ color: 'black', fontSize: 25, fontWeight: 'bold' }}>HOME</Text>
                 </View>
                 <View style={styles.buttonView}>
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={styles.button}
-                        onPress={() => navigation.navigate("Um") } 
+                        onPress={() => navigation.navigate("Um")}
                     >
                         <Text style={styles.buttonText}>Um</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={styles.button}
-                        onPress={() => navigation.navigate("Dois") } 
+                        onPress={() => navigation.navigate("Dois")}
                     >
                         <Text style={styles.buttonText}>Dois</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.buttonView}>
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={styles.button}
-                        onPress={() => navigation.navigate("Tres") } 
+                        onPress={() => navigation.navigate("Tres")}
                     >
                         <Text style={styles.buttonText}>Três</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={styles.button}
-                        onPress={() => navigation.navigate("Quatro") } 
+                        onPress={() => navigation.navigate("Quatro")}
                     >
                         <Text style={styles.buttonText}>Quatro</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.buttonView}>
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={styles.button}
-                        onPress={() => navigation.navigate("Cinco") } 
+                        onPress={() => navigation.navigate("Cinco")}
                     >
                         <Text style={styles.buttonText}>Cinco</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={styles.button}
-                        onPress={() => navigation.navigate("Seis") } 
+                        onPress={() => navigation.navigate("Seis")}
                     >
                         <Text style={styles.buttonText}>Seis</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.buttonView}>
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={styles.button}
-                        onPress={() => navigation.navigate("Sete") } 
+                        onPress={() => navigation.navigate("Sete")}
                     >
                         <Text style={styles.buttonText}>Sete</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={styles.button}
-                        onPress={() => navigation.navigate("Oito") } 
+                        onPress={() => navigation.navigate("Oito")}
                     >
                         <Text style={styles.buttonText}>Oito</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.buttonView}>
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={styles.button}
-                        onPress={() => navigation.navigate("Nove") } 
+                        onPress={() => navigation.navigate("Nove")}
                     >
                         <Text style={styles.buttonText}>Nove</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={styles.button}
-                        onPress={() => navigation.navigate("Dez") } 
+                        onPress={() => navigation.navigate("Dez")}
                     >
                         <Text style={styles.buttonText}>Dez</Text>
                     </TouchableOpacity>
